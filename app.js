226d3fad5020ecb285cef4c3731f8e271d7bbc8d
Shubham <shubhamsingh705010@gmail.com>
2024-02-16 19:25:25 +0530

    event listner

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const height = document.querySelector("#height")
 const weight = document.querySelector("#weight")
 const submit = document.querySelector("#submit")
+
+function bmi() {
+    alert("Hello ji")
+}
+
+submit.addEventListener("click", bmi)
